02bcdfe98b7ca1c12a6b6a5265828581b8fc6932
Greg Gonzalez <gregery.gonzalez@gmail.com>
2021-04-29 22:03:04 -0400

    Initial Greg

---
+++ 72_Hours/Models/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _72_Hours.Models
+{
+    public class Test
+    {
+        //test test test test test
+    }
+}
