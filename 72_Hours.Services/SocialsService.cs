b698e1e880df514ef64e3b1eeb350c412441e78b
ksgraves <kraegan.s.g@gmail.com>
2021-05-01 11:01:52 -0400

    Service Layers and new methods added

--- 72_Hours.Services/SocialsService.cs
+++ 72_Hours.Services/SocialsService.cs
@@ -9,65 +9,15 @@ namespace _72_Hours.Services
     class SocialsService
     {
 
-        //POST METHODS
-
-        //Create Post
-        public bool CreatePost(PostCreate model)
-        {
-            var entity = new Post()
-            {
-                OwnerId = _userId,
-                Title = model.Title,
-                Content = model.Content,
-                CreatedUtc = DateTimeOffset.Now
-            };
-
-            using (var ctx = new ApplicationDbContext())
-            {
-                ctx.Notes.Add(entity);
-                return ctx.SaveChanges() == 1;
-            }
-        }
-
-        //Get All Posts
-        public IEnumerable<NoteListItem> GetAllPosts()
-        {
-            using (var ctx = new ApplicationDbContext())
-            {
-                var query =
-                    ctx
-                    .Notes
-                    .Where(e => e.OwnerId == _userId)
-                    .Select(
-                        e =>
-                        new NoteListItem
-                        {
-                            NotedID = e.NoteId,
-                            Title = e.Title,
-                            CreatedUtc = e.CreatedUtc
-                        }
-                    );
-
-                return query.ToArray();
-
-            }
-        }
-
-        //COMMENT METHODS
-        // Create Comment on Post (using Foregn Key Relationship) 
-
-        // Get comment by Post Id 
-
-        //REPLY METHODS
-        //Create a reply toa comment using a foreign key relationship
-
-        // Get Replies By Commend Id 
 
 
-        // LIKE METHODS 
-        // Create a Like on a post using a foreign key relationship 
+     
 
-        // Get Likes by Post Id 
+      
+
+
+
+        
 
 
     }
